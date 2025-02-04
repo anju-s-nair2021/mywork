f1c79c9fe8c781c492c97c39355facdc62e2d3ee
Anju <anjusreeni2021@gmail.com>
2025-02-04 10:37:34 -0800

    final

--- e-comm/venv/mypro/mypro/settings.py
+++ e-comm/venv/mypro/mypro/settings.py
@@ -14,10 +14,12 @@ from pathlib import Path
 # import environ
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import sys
+sys.path.append(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
